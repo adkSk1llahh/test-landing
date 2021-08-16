51d8be91585ede8fc547a09db2bee51b7e3a1329
Adilkhan Zhumabekov <sk1llahh2@gmail.com>
2021-08-16 09:18:19 +0600

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,58 @@
+for (const dropdown of document.querySelectorAll(".navbar-select-wrapper")) {
+    dropdown.addEventListener('click', function () {
+        this.querySelector('.navbar-select').classList.toggle('open');
+    })
+}
+window.addEventListener('click', function (e) {
+    for (const select of document.querySelectorAll('.navbar-select')) {
+        if (!select.contains(e.target)) {
+            select.classList.remove('open');
+        }
+    }
+});
+for (const option of document.querySelectorAll(".navbar-option")) {
+    option.addEventListener('click', function () {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.navbar-option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.navbar-select').querySelector('.navbar-select__trigger span').textContent = this.textContent;
+        }
+    })
+}
+
+var acc = document.getElementsByClassName("faq-item__text");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+var menuButton = document.querySelector('.menu-button');
+var menuList = document.querySelector('.main-menu');
+var menuCloseButton = document.querySelector('.menu-close-button');
+var body = document.querySelector('body');
+
+
+
+menuButton.addEventListener('click', function () {
+    menuList.classList.toggle('open');
+    body.classList.toggle('overlay');
+});
+
+while (menuList.length) {
+    menuCloseButton.addEventListener('click', function () {
+        menuList[0].classList.remove('open');
+    });
+    
+}
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+var gulp = require("gulp");
+var cssnano = require("gulp-cssnano");
+var autoprefixer = require("gulp-autoprefixer");
+var cleanCSS = require("gulp-clean-css");
+purgecss = require("gulp-purgecss");
+var imagemin = require("gulp-imagemin");
+var sass = require("gulp-sass")(require("sass"));
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var browserSync = require("browser-sync").create();
+var sourcemaps = require("gulp-sourcemaps");
+var del = require("del");
+
+const browserSupport = [
+  "last 2 version",
+  "safari 5",
+  "ie 8",
+  "ie 9",
+  "opera 12.1",
+  "ios 6",
+  "android 4",
+];
+
+gulp.task("sass", function () {
+  return gulp
+    .src("./app/scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer(browserSupport))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./app/css/"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
+});
+
+// copy html
+gulp.task("html", function html() {
+  return gulp.src("./app/*.html").pipe(gulp.dest("./dist/"));
+});
+
+// copy css
+gulp.task("copycss", function html() {
+  return gulp
+    .src("./app/css/*.css")
+    .pipe(
+      purgecss({
+        content: ["./app/*.html", "./app/js/*.js"],
+      })
+    )
+    .pipe(cssnano())
+    .pipe(gulp.dest("./dist/css/"));
+});
+
+// copy js
+gulp.task("copyjs", function () {
+  return gulp.src("./app/js/*.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("./dist/js"));
+});
+
+//copy fonts
+gulp.task('fonts', function() {
+  return gulp.src('./app/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts'))
+})
+
+// copy img
+gulp.task("images", function html() {
+  return gulp
+    .src("./app/**/*.{gif,jpg,png,svg,jpeg}")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/"));
+});
+
+// Static server
+gulp.task("serve", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./app",
+    },
+  });
+});
+
+// watch sass
+gulp.task("watch", ["serve", "sass"], function (done) {
+  gulp.watch("app/scss/**/*.scss", ["sass"]);
+  gulp.watch("app/*.html").on("change", browserSync.reload);
+  done();
+});
+
+// clean dist folder
+gulp.task("clean:dist", function () {
+  return del.sync("dist");
+});
+
+gulp.task("build", ['clean:dist','copycss','html','images','copyjs', 'fonts'], function(done){
+  console.log("======================================================");
+  console.log("==================BUILD SUCCESSFULL===================");
+  console.log("======================================================");
+  done();
+})

66b18b9f80a504215bace65dbb9ae7f03e1efea2
Adilkhan Zhumabekov <sk1llahh2@gmail.com>
2021-08-16 15:40:48 +0600

    second commit

---
+++ app/js/form.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+    $('#button-question').click(function () {
+        $('.modal-form').toggleClass('visible');
+    });
+
+    $('.form-close-button').click(function () {
+        $('.modal-form').toggleClass('visible');
+    });
+
+
+    $("#form").validate({
+        rules: {
+            user_name: {
+                required: true,
+            },
+            user_email: {
+                required: true,
+                email: true
+            },
+            user_message: {
+                required: true,
+            },
+        },
+        messages: {
+            user_name: {
+                required: "Это поле обязательно для заполнения",
+            },
+            user_email: {
+                required: "Это поле обязательно для заполнения",
+                email: "Не правильный адрес",
+            },
+            user_message: {
+                required: "Заполните форму",
+            },
+        },
+
+        submitHandler: function(form) {
+            $.ajax({
+                url: 'https://enca877xn1qkmz6.m.pipedream.net',
+                type: 'POST',
+                dataType: 'json',
+                success: function () {
+                    $('.form-content').addClass('hidden');
+                    $('.form-content-success').addClass('visible');
+                },
+                error: function () {
+                    $('.form-content').addClass('hidden');
+                    $('.form-content-error').addClass('visible');
+                }
+            });
+            console.log
+        }
+    });
+
+});
+
